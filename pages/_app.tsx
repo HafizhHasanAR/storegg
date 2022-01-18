dc22b00089e985b48cfc13605adc061405226f13
HafizhHasanAR <hafizhhafizh9@gmail.com>
2022-01-18 23:31:58 +0700

    menambakan SignIn dan SignUp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,10 @@ import '../styles/utilities.css'
 import '../styles/homepage.css'
 import '../styles/checkout.css'
 import '../styles/complete-checkout.css'
+import '../styles/sign-in.css'
+import '../styles/sign-up.css'
+
+
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
